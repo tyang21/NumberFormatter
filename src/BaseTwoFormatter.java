474848d00b1e80d3ee0438d5a1bbcfde2e452da7
tyang21 <42978521+tyang21@users.noreply.github.com>
2019-01-13 23:02:50 -0500

    Add files via upload

--- src/BaseTwoFormatter.java
+++ src/BaseTwoFormatter.java
@@ -1,4 +1,22 @@
 
-public class BaseTwoFormatter {
-
+public class BaseTwoFormatter 
+{
+	public String format(int p)
+	{
+		String s = "";
+		while(p/2 != 0)
+		{
+			s += p%2;
+			p = p/2;
+		}
+		s += p%2;
+		String c = "";
+		{
+			for(int i = 1; i < s.length() + 1; i++)
+			{
+				c += s.charAt(s.length()-i);
+			}
+		}
+		return c;
+	}
 }
